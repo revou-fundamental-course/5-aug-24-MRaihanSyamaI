b4dab1ff784169e380467cab09133cee54858198
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2024-08-14 09:09:34 +0700

    last update

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@ let hasilPerhitungan = document.getElementById("hasilPerhitungan");
 let btnInput = document.getElementById("btnInput");
 let btnReset = document.getElementById("btnReset");
 let statusHasil = document.getElementById("statusHasil");
+let statusHasilKet = document.getElementById("statusHasilKet");
+let statusRange = document.getElementById("statusRange");
 let hasil = 0;
 let statusBB;
 
 btnReset.onclick = function() {
     hasil = 0;
+    statusBB = "-";
+    statusRange = "-";
     hasilPerhitungan.textContent = hasil;
+    statusHasil.textContent = statusBB; 
+    statusHasilKet.textContent = statusBB; 
+    statusRange.textContent = statusRange;
 }
 
 btnInput.onclick = function () {
@@ -19,7 +26,10 @@ btnInput.onclick = function () {
     tinggiBadan = Number(tinggiBadan);
     tinggiBadan = tinggiBadan / 100;
     hasil = beratBadan / tinggiBadan ** 2;
+    let min = hasil - 1;
+    let max = hasil + 1;
     hasilPerhitungan.textContent = hasil.toFixed(1);
+    statusRange.textContent = `${min.toFixed(0)} dan ${max.toFixed(0)}`;
 
     if (hasil < 18.5) {
         statusBB = "Kekurangan";
@@ -30,6 +40,7 @@ btnInput.onclick = function () {
     } else {
         statusBB = "Obesitas";
     }
-
-    statusHasil.textContent = statusBB; 
+    
+    statusHasil.textContent = statusBB;
+    statusHasilKet.textContent = statusBB; 
 }
