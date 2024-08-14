910620a8d019aa2e03a8c23bf77520572294194c
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2024-08-14 20:34:05 +0700

    add comment

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let statusRange = document.getElementById("statusRange");
 let hasil = 0;
 let statusBB;
 
+// Memberikan fungsi btnReset 
 btnReset.onclick = function() {
     hasil = 0;
     statusBB = "-";
@@ -19,18 +20,25 @@ btnReset.onclick = function() {
     statusRange.textContent = statusRange;
 }
 
+// Memberikan fungsi btnInput
 btnInput.onclick = function () {
+    // Menggambil Value dari input
     beratBadan = beratBadan.value;
-    beratBadan = Number(beratBadan);
     tinggiBadan = tinggiBadan.value;
+    // Mengubah dari string (karena input) ke Number
+    beratBadan = Number(beratBadan);
     tinggiBadan = Number(tinggiBadan);
+    // Menggubah dari cm ke m
     tinggiBadan = tinggiBadan / 100;
+    // Perhitungan
     hasil = beratBadan / tinggiBadan ** 2;
+    // Memberikan renge hasil
     let min = hasil - 1;
     let max = hasil + 1;
-    hasilPerhitungan.textContent = hasil.toFixed(1);
+    hasilPerhitungan.textContent = hasil.toFixed(1); // Agar mendapatkan 1 angka dibelakang koma
     statusRange.textContent = `${min.toFixed(0)} dan ${max.toFixed(0)}`;
 
+    // Agar dapat dikelompokan sesuai hasil
     if (hasil < 18.5) {
         statusBB = "Kekurangan";
     } else if (hasil >= 18.5 && hasil <= 24.9) {
